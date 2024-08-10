fce63be6aee89f9d676ede26b20dc91968c2dbe6
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-10 16:52:02 -0400

    passing tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import { useState, useEffect } from "react"// create your App component here
+
+function App() {
+    const [dogImage, setDogImage] = useState("")
+    const [isLoading, setIstLoading] = useState(true)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setDogImage(data.message)
+                setIstLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+
+    console.log(dogImage)
+
+    return (
+        <img src={dogImage} alt="A Random Dog" ></img>
+    )
+}
+
+export default App
